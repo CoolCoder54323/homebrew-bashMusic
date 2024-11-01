e669b12c374d12bdcaa94769a6cd34da9a010e09
nicholasdixon <nicxellent@icloud.com>
2024-11-01 18:11:23 -0400

    changed back to one file

--- bashPlay.sh
+++ bashPlay.sh
@@ -1,6 +1,30 @@
-#Check dependencies
+#!/bin/bash/
 
-source ./checkDeps.sh
+codeName=$(uname -s)
+case $codeName in
+	Darwin)
+		OS=mac
+		;;
+	Linux)
+		OS=linux
+		;;
+	*)
+		OS=linux
+		;;
+esac
+for dep in sox ffmpeg; do
+	check=$(whereis -q $dep)
+	if [ -z "$check" ]; then
+		echo "'$dep' is not installed. Would you like to (yes/no)"
+		read -n 10 install
+		if [ "yes" = $install ]; then
+			[ $OS = linux ] && sudo apt-get -y install $dep || [ $OS = mac ] && echo "mac install" && brew install $dep 
+		else
+			exit 1
+		fi
+
+	fi
+done
 
 getArg() {
 	echo "$RESPONSE" | sed -n "s/.*\"$1\":\([\"]*[^\",]*[\"]*\).*/\1/p" | xargs echo
--- checkDeps.sh
+++ checkDeps.sh
@@ -1,9 +1,6 @@
-#!/bin/bash/
 
-#Check dependencies
 
 codeName=$(uname -s)
-echo $OS
 case $codeName in
 	Darwin)
 		OS=mac
@@ -15,14 +12,12 @@ case $codeName in
 		OS=linux
 		;;
 esac
-echo $OS
 for dep in sox ffmpeg; do
 	check=$(whereis -q $dep)
 	if [ -z "$check" ]; then
 		echo "'$dep' is not installed. Would you like to (yes/no)"
 		read -n 10 install
-		if [ "yes" = $install ];then
-			echo "$dep dkpdwddw"
+		if [ "yes" = $install ]; then
 			[ $OS = linux ] && sudo apt-get -y install $dep || [ $OS = mac ] && echo "mac install" && brew install $dep 
 		else
 			exit 1
